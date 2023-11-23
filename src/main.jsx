4525dd13ebd0c1bfb0ef520b48fc603ba5dbc20c
Lup1in <gabrielcunhaleite@hotmail.com>
2023-11-23 20:30:30 -0300

    moviecard, moviecard css, navbar css

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App.jsx'
 
+import App from './App.jsx'
 import Home from './pages/Home.jsx';
 import Movie from './pages/Movie.jsx';
 import Search from './pages/Search.jsx';
@@ -19,8 +19,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Route path='search' element={<Search />} />
         </Route>
       </Routes>
-
-
     </BrowserRouter>
   </React.StrictMode>,
-)
+);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,37 @@
 import { useState, useEffect } from "react"
+import MovieCard from '../Components/MovieCard'
+import './MovieGrid.css'
 
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
 
 const Home = () => {
-    const [topMovies, setTopMovies] = useState([])
+    const [topMovies, setTopMovies] = useState([]);
 
     const getTopRatedMovies = async (url) => {
-        const res = await fetch(url)
-        const data = await res.json()
-
+        const res = await fetch(url);
+        const data = await res.json();
         setTopMovies(data.results);
     };
 
     useEffect(() => {
         const topRatedURL = `${moviesURL}top_rated?${apiKey}`;
-
+        console.log(topRatedURL);
         getTopRatedMovies(topRatedURL);
-
     }, []);
+
+    console.log(topMovies);
 
     return (
         <div className="container">
             <h2 className="title">Melhores Filmes</h2>
-            {topMovies.length === 0 && <p>Carregando...</p>}
-            <div className="movies-container">{topMovies.length > 0 && topMovies.map((movie) => <p>{movie.title}</p>)}</div>
-
+            <div className="movies-container">
+                {topMovies.length === 0 && <p>Carregando...</p>}
+                {topMovies.length > 0 &&
+                    topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+            </div>
         </div>
-    )
+    );
 };
 
 export default Home
